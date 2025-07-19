a05a4520dbf4a2b48a6d845cf1f0b3773acc4aef
pateluday07 <pateluday07@gmail.com>
2025-07-20 02:30:25 +0530

    remove unused imports

--- src/main/java/com/pateluday07/client/CareTipClient.java
+++ src/main/java/com/pateluday07/client/CareTipClient.java
@@ -3,7 +3,6 @@ package com.pateluday07.client;
 import com.pateluday07.dto.CareTipRequestDTO;
 import com.pateluday07.dto.CareTipResponseDTO;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
